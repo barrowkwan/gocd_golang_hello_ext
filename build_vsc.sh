07d34b06f253b886652688a7aa99decdf7cafd91
Barrow Kwan <barrowkwan@gmail.com>
2016-08-09 21:19:22 -0700

    go get without install

--- build_vsc.sh
+++ build_vsc.sh
@@ -5,7 +5,32 @@ export GOPATH
 
 rm -rf $GOPATH/bin/*
 
-go test -v github.com/barrowkwan/gocd_golang_hello_ext/country
+if [[ -f $GOPATH/bin/go-junit-report ]]; then
+  echo "Remove binary : $GOPATH/bin/go-junit-report"
+  rm -rf $GOPATH/bin/go-junit-report
+fi
+if [[ -d src/github.com/jstemmer/go-junit-report ]]; then
+  echo "Remove external library : github.com/jstemmer/go-junit-report"
+  rm -rf src/github.com/jstemmer/go-junit-report
+fi
+echo "Get github.com/jstemmer/go-junit-report"
+go get -d github.com/jstemmer/go-junit-report
 
-GOOS=darwin go build -o $GOPATH/bin/helloworld github.com/barrowkwan/gocd_golang_hello_ext
+go build -o $GOPATH/bin/go-junit-report github.com/jstemmer/go-junit-report
+if [[ -f testreport.xml ]]; then
+  echo "Remove old test report : testreport.xml"
+  /bin/rm -rf testreport.xml
+fi
+go test -v github.com/barrowkwan/gocd_golang_hello_ext/... | $GOPATH/bin/go-junit-report > testreport.xml
 
+
+echo "======================="
+echo "Building binary for OSX"
+GOOS=darwin go build -o $GOPATH/bin/helloworld_osx github.com/barrowkwan/gocd_golang_hello_ext
+echo "======================="
+
+
+echo "======================="
+echo "Building binary for linux"
+GOOS=linux go build -o $GOPATH/bin/helloworld_linux github.com/barrowkwan/gocd_golang_hello_ext
+echo "======================="
